603b4fc87623f412b73af4e3903b834fa3853523
Precious <preshengr@mail.com>
2023-07-19 23:28:18 +0100

    Variables_if_else

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,13 +7,17 @@
  */
 int main(void)
 {
-	int ch;
+int ch;
 
-	for (ch = 97; ch <= 122; ch++)
-	{
-		putchar(ch);
-	}
-	putchar(10); /* this is an ascii code for new line*/
+for (ch = 97; ch <= 122; ch++)
+{
+putchar(ch);
+}
+for (ch = 65; ch <= 90; ch++)
+{
+putchar(ch);
+}
+putchar(10); /* this is an ascii code for new line*/
 
-	return (0);
+return (0);
 }
