4482ffde9af3419079f25695ed3ef616bb96e6fb
Precious <preshengr@mail.com>
2023-07-19 23:29:18 +0100

    Variables_if_else

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,13 +7,16 @@
  */
 int main(void)
 {
-	int ch;
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch != 'q' && ch != 'e')
+{
+putchar(ch);
+}
+}
 
-	for (ch = 97; ch <= 122; ch++)
-	{
-		putchar(ch);
-	}
-	putchar(10); /* this is an ascii code for new line*/
+putchar(10); /* this is an ascii code for new line*/
 
-	return (0);
+return (0);
 }
